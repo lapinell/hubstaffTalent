a2047047372d6bbe283453b7288102de725f9190
lapinell <lapinell@gmail.com>
2018-06-07 22:22:17 -0500

    installed SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,20 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+          loader: "style-loader"
+          }, 
+          {
+          loader: "css-loader" 
+          }, 
+          {
+          loader: "sass-loader"
+          }
+        ]
       }
     ]
   },
