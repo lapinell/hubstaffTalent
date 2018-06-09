c9bfb351fa8e7565918041b2fe0c758de7546c15
lapinell <lapinell@gmail.com>
2018-06-09 03:41:30 -0500

    fixed misconfigurations by utilizing create-react-app configurations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component} from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Test from './components/Nav'
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <Test />
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
