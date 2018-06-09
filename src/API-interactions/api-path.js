27862516cb7eada32accc3b742bb4a334cbe2edc
lapinell <lapinell@gmail.com>
2018-06-09 11:41:29 -0500

    created api path functions and individual query calls for filtering

--- src/API-interactions/api-path.js
+++ src/API-interactions/api-path.js
@@ -1,1 +1,16 @@
-export default const APIpath = "https://jobs.github.com/positions.json";
+import axios from 'axios';
+
+const APIpath = "https://jobs.github.com/positions.json";
+
+const apiGET = (query) => { //query is an endpoint in string format
+    return axios.get(APIpath + '?' + query)
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+};
+
+export default APIpath;
+export { apiGET }
--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/Nav'
+import Test from './components/Nav';
+import { Search, searchLatLong, searchLocation, searchFT, GETposition } from './API-interactions/api-calls';
+
+Search("ux");
+searchLatLong(37.3229978, -122.0321823);
+searchLocation(37211);
+searchLocation("Cupertino");
+searchFT();
 
 class App extends Component {
   render() {
