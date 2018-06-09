47d32d17564306fff115da9c4e978940e83c31ac
lapinell <lapinell@gmail.com>
2018-06-09 11:42:50 -0500

    removed unneeded import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/Nav';
-import { Search, searchLatLong, searchLocation, searchFT, GETposition } from './API-interactions/api-calls';
 
 class App extends Component {
   render() {
