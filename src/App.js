b600cbdd6fa049189e3e55d8a88c22461bb45d39
lapinell <lapinell@gmail.com>
2018-06-09 11:42:27 -0500

    removed test code

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import './App.css';
 import Test from './components/Nav';
 import { Search, searchLatLong, searchLocation, searchFT, GETposition } from './API-interactions/api-calls';
 
-Search("ux");
-searchLatLong(37.3229978, -122.0321823);
-searchLocation(37211);
-searchLocation("Cupertino");
-searchFT();
-
 class App extends Component {
   render() {
     return (
