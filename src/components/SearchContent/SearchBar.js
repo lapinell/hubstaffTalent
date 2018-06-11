95d18d3d75e6401132fecafb94f33c9ebbe2e8ab
lapinell <lapinell@gmail.com>
2018-06-11 12:22:53 -0500

    styled search bar

--- src/components/SearchContent/SearchBar.js
+++ src/components/SearchContent/SearchBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Row, Col, Input} from 'antd';
 import 'antd/dist/antd.css';
+import './SearchBar.css';
 
 const Search = Input.Search;
 
 class SearchBar extends React.Component {
     render() {
         return (
-             <Row>
+             <Row id="searchBar">
                 <Col span={16} offset={4}>
-                    <Search
+                    <Search id="searchBox"
                     placeholder="input search text"
                     enterButton="Search"
                     size="large"
