62ab9a450223a12996b5fbf16fbb684600936057
lapinell <lapinell@gmail.com>
2018-06-12 09:43:42 -0500

    styled individual results

--- src/components/SearchContent/Results/SingleResult.js
+++ src/components/SearchContent/Results/SingleResult.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Badge } from 'antd';
+import './SingleResult.css';
 
 export default class SingleResult extends React.Component {
 
@@ -10,15 +11,15 @@ export default class SingleResult extends React.Component {
 
         let skillBadges = skills.map((skill) => {
 
-          return <Badge count={skill} style={{ backgroundColor: '#fff', color: '#999', boxShadow: '0 0 0 1px #d9d9d9 inset'}} />
+          return <Badge className="badge-skill" count={skill} style={{ backgroundColor: '#fff', color: '#999', boxShadow: '0 0 0 1px #d9d9d9 inset'}} />
 
         });
 
         return (
             <div className="jobResult">
                 <div class="jobTitle">
-                    <p>{this.props.title}</p>
-                    <Badge count={this.props.availability} />
+                    <p className="title">{this.props.title}</p>
+                    <Badge className="badge-availability" count={this.props.availability} />
                 </div>
                 <div className="jobCompany">
                     <p className="company"><a href="#">{this.props.company}</a></p>
